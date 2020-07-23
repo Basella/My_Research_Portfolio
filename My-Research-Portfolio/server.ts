32f013ea4e1f53371ba1de8aaca5a4b550ebc612
Emmanuela <emmanuela.ezeakacha@xputertechnologies.com>
2020-07-23 08:44:44 +0100

    fixing "page not working"

--- My-Research-Portfolio/server.ts
+++ My-Research-Portfolio/server.ts
@@ -9,10 +9,10 @@ app.use('/css', express.static(__dirname + '/css'));
 app.use('/partials', express.static(__dirname + '/partials'));
 app.use(express.static('./dist/My-Research-Portfolio'));
 
-app.use('/*', (req, res, next) => {
+app.all('/*', (req, res, next) => {
     // Just send the index.html for other files to support HTML5Mode
     // res.sendFile('index.html', { root: __dirname + '/src/' });
-    res.sendFile(path.join(ROOTDIR, 'dist/my-research-portfolio/index.html'));
+    res.sendFile(path.join(ROOTDIR, 'dist/My-Research-Portfolio/index.html'));
 });
 
 // app.use(express.static('./dist/My-Research-Portfolio'));

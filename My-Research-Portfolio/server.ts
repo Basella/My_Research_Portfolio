6271e04a30807620a8d015a7896d0c83949e4c14
Emmanuela <emmanuela.ezeakacha@xputertechnologies.com>
2020-07-23 10:00:15 +0100

    fixing "page not working"

--- My-Research-Portfolio/server.ts
+++ My-Research-Portfolio/server.ts
@@ -3,25 +3,26 @@ const path = require('path');
 const ROOTDIR = path.join(__dirname);
 const app = express();
 
-app.use('/js', express.static(__dirname + '/js'));
-app.use('/dist', express.static(__dirname + '/../dist'));
-app.use('/css', express.static(__dirname + '/css'));
-app.use('/partials', express.static(__dirname + '/partials'));
-app.use(express.static('./dist/My-Research-Portfolio'));
+// app.use('/js', express.static(__dirname + '/js'));
+// app.use('/dist', express.static(__dirname + '/../dist'));
+// app.use('/css', express.static(__dirname + '/css'));
+// app.use('/partials', express.static(__dirname + '/partials'));
+// app.use(express.static(__dirname + '/src/app'));
+// app.use(express.static(__dirname + '/src/app/app.component.html'));
 
-app.all('/*', (req, res, next) => {
-    // Just send the index.html for other files to support HTML5Mode
-    // res.sendFile('index.html', { root: __dirname + '/src/' });
-    res.sendFile(path.join(ROOTDIR, 'dist/My-Research-Portfolio/index.html'));
-});
-
-// app.use(express.static('./dist/My-Research-Portfolio'));
-// // app.use(express.static('./src/index.html'));
-
-// app.use((req, res) => {
-//   res.sendFile(path.join(ROOTDIR, 'dist/my-research-portfolio/index.html'));
-//   // res.sendFile('app.component.html', { root: 'src/app/'});
+// app.all('/*', (req, res, next) => {
+//     // Just send the index.html for other files to support HTML5Mode
+//     res.sendFile('app.component.html', { root: __dirname + '/src/app/' });
+//     // res.sendFile(path.join(ROOTDIR, 'dist/My-Research-Portfolio/index.html'));
 // });
+
+app.use(express.static('./dist/My-Research-Portfolio'));
+// app.use(express.static('./src/index.html'));
+
+app.use((req, res) => {
+  res.sendFile(path.join(ROOTDIR, '/dist/my-research-portfolio/index.html'));
+  // res.sendFile('app.component.html', { root: 'src/app/'});
+});
 
 app.listen(8080);
 console.log('Listening on port ' + 8080);

bc56a82caf4529a61d8734a1749c0707c1a896a6
Emmanuela <emmanuela.ezeakacha@xputertechnologies.com>
2020-07-24 10:36:57 +0100

    fixing

--- My-Research-Portfolio/server.ts
+++ My-Research-Portfolio/server.ts
@@ -15,7 +15,7 @@ const app = express();
 //     // res.sendFile(path.join(ROOTDIR, 'dist/My-Research-Portfolio/index.html'));
 // });
 
-app.use(express.static(__dirname + '/dist/My-Research-Portfolio'));
+app.use(express.static(__dirname +  './dist/My-Research-Portfolio'));
 // app.use('/docs', express.static('./src/app/undergrad-docs/undergrad-docs.component.html'));
 
 
@@ -47,7 +47,7 @@ app.use(express.static(__dirname + '/dist/My-Research-Portfolio'));
 // app.use(express.static('./src/index.html'));
 
 app.use('/*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/dist/my-research-portfolio/index.html'));
+  res.sendFile(path.join(__dirname + './dist/my-research-portfolio/index.html'));
   // res.sendFile(path.join(ROOTDIR, 'src/app/mainpage/mainpage.component.html'));
   // res.sendFile('app.component.html', { root: 'src/app/'});
 });
